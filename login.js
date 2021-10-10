e180f3f666e0f8db9801d2916183409b442ec363
vio putri yuwandika <46666598+vioputriy@users.noreply.github.com>
2021-10-10 23:51:30 +0700

    Update login.js

--- login.js
+++ login.js
@@ -35,5 +35,63 @@ describe('Scenario 01(web-admin)', function(){
         cy.get('#navbarDropdown').click();
         cy.get('[href="/profile"]').click();
         cy.get('.w-150px').click();
+    }), 
+    it('TC-15', function() {
+        cy.get('#navbarDropdown').click();
+        cy.get('[href="/settings"]').click();
+        cy.get('.col-md-9 > .card > .card-header > .pt-2 > b')
+            .should('exist', 'Username & Password');
+    }),
+    it('TC-16', function() {
+        cy.get('#navbarDropdown').click();
+        cy.get('[href="/settings"]').click();
+        cy.get('li.large-font > .link-white').click();
+        cy.get('.col-md-9 > .card > .card-header > .pt-2 > b').should('exist', 'Report Problem');
+    }), 
+    it('TC - 17 - HomePage',function(){
+        cy.get(':nth-child(1) > .nav-link').click();
+        cy.get('input').type('Quality Control {enter}');
+    }),
+    it('TC-18-Creat Company',function(){
+            cy.get('#navbarDropdown').click();
+            cy.get('.dropdown-menu > .cursor-pointer').click({force:true});
+            cy.get('.btn').click();
+        }),
+        it('TC-19', function() {
+            cy.get('#navbarDropdown').click()
+            cy.get('.dropdown-menu > .cursor-pointer').click({force:true});
+            cy.get('.btn').click();
+            cy.get('#province').select('JAWA TIMUR', {force: true}).should('exist', 'JAWA TIMUR');
+            cy.get('#city').select('KOTA MALANG', {force:true}).should('exist', 'KOTA MALANG');
+            cy.get('.btn').click();
+        }),
+        it('TC-20', function() {
+            cy.get('#navbarDropdown').click();
+            cy.get('.dropdown-menu > .cursor-pointer').click({force:true});
+            cy.get('.btn').click();
+            cy.get('#company_name').type('coba').should('have.value', 'coba');
+            cy.get('.btn').click();
+            cy.get(':nth-child(2) > .w-100 > .text-danger');
+                .should('exist', 'The province field is required.');
+            cy.get(':nth-child(3) > .w-100 > .text-danger');
+                .should('exist', 'The city field is required.');
+        }),
+        it('TC-21', function() {
+            cy.get('#navbarDropdown').click();
+            cy.get('.dropdown-menu > .cursor-pointer').click({force:true});
+            cy.get('#company_name').type('ABC3').should('have.value', 'ABC3');
+            cy.get('#province').select('JAWA TIMUR', {force: true}).should('exist', 'JAWA TIMUR');
+            cy.get('#city').select('KOTA MALANG', {force:true}).should('exist', 'KOTA MALANG');
+            cy.get('.text-muted').click();
+        }),
+        it('TC-22-Search JOB', function() {
+            cy.get(':nth-child(2) > .nav-link').click();
+            cy.get('.p-4').type('Quality Control{enter}');
+            cy.get('.form-group > .normal-font').click();
+        }), 
+        it('TC-23-My Applicant', function() {
+            cy.get('#navbarDropdown').click();
+            cy.get('[href="/my-applicant"]').click();
+            cy.get('[data-id="Submitted"]').click();
+        })
     })
-}) 
